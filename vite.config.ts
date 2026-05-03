48d3c0e8257d4419f7be794f8843c7a52e2bb9ba
IP <41252928+x0r-hash@users.noreply.github.com>
2026-05-03 23:27:24 +0200

    Replace react-start with tanstackRouter plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { start } from '@tanstack/react-start'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
-    start({
-      adapter: 'node',
-    }),
+    tanstackRouter(),
     react(),
   ],
 })
