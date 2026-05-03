6cef8227eb9eff0a76887bbce7702383b81633f4
IP <41252928+x0r-hash@users.noreply.github.com>
2026-05-03 23:18:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { tanstackStart } from '@tanstack/start/vite'
+import { tanstackStart } from '@tanstack/react-start/vite'
 
 export default defineConfig({
   plugins: [
